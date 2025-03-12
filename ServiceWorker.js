56215853dea2e72d12d9c29824b971dac9bfb0b5
iNapkin <shadowpuppyfighting@gmail.com>
2025-03-12 16:55:23 -0600

    add game

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "PotionScape-node_builder-1.0";
+const contentToCache = [
+    "Build/Node_Builder.loader.js",
+    "Build/Node_Builder.framework.js.br",
+    "Build/Node_Builder.data.br",
+    "Build/Node_Builder.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
