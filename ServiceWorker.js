5d01126527c3154e793e1b464c6b5e71d186c1b0
iNapkin <shadowpuppyfighting@gmail.com>
2025-03-24 07:52:39 -0600

    change

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "PotionScape-node_builder-1.0";
+const cacheName = "PotionScape-node builder-0.1";
 const contentToCache = [
     "Build/Node_Builder.loader.js",
     "Build/Node_Builder.framework.js",
